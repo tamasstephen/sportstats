ca01ac9bbe266fd76b3012ace59d65826e855887
tamasstephen <kicsitnagyon@gmail.com>
2022-09-26 20:51:10 +0200

    refactor: Add response enum

---
+++ src/model/status.ts
@@ -0,0 +1,4 @@
+export enum ResponseJsonStatus {
+  OK = "ok",
+  ERROR = "error",
+}
--- src/routes/premLeague.ts
+++ src/routes/premLeague.ts
@@ -2,19 +2,23 @@ import { response, Router } from "express";
 import { FootBallMatchData } from "../services/football";
 import { getCsv } from "../utils/csv";
 import { MatchDataStringObj } from "../model/matchData";
+import { ResponseJsonStatus } from "../model/status";
 
 export const router = Router();
 
 router.get("/", async (req, res) => {
   const dataSource = new FootBallMatchData(await getCsv());
   const team = req.query.team;
-  const responseJson:
-    | { [matches: string]: MatchDataStringObj[] }
-    | { [error: string]: string } = {};
+  const responseJson: Partial<{
+    matches: MatchDataStringObj[];
+    status: string;
+    error: string;
+  }> = { status: ResponseJsonStatus.OK };
   if (typeof team === "string") {
     responseJson["matches"] = dataSource.getMatchesByTeam(team);
   } else {
     responseJson["error"] = "Please provide a string as a team name";
+    responseJson.status = ResponseJsonStatus.ERROR;
   }
   res.header("Content-Type", "application/json");
   res.end(JSON.stringify(responseJson));
